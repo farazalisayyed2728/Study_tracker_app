3f7fd9d6951e0f58cf830c6365411bb5c939ce8b
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-08-23 19:46:23 +0530

    remove comments

--- mystudytracker/mystudytracker/settings.py
+++ mystudytracker/mystudytracker/settings.py
@@ -106,9 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -116,20 +113,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-
-
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# SECURE_BROWSER_XSS_FILTER = True
-# SECURE_CONTENT_TYPE_NOSNIFF = True
-# Make sure these are False (or completely commented out) in local development
-
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
